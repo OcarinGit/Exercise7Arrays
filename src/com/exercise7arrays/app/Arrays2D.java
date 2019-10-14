5467fbcc03428c0cc514407f16173dac890a8834
OcarinGit <oscarsantanaalvarez@gmail.com>
2019-10-14 16:54:31 -0500

    Projects on 1darrays and 2d Arrays

---
+++ src/com/exercise7arrays/app/Arrays2D.java
@@ -0,0 +1,34 @@
+package com.exercise7arrays.app;
+
+public class Arrays2D {
+
+	public static void main(String[] args) {
+		//Constants declaration
+		final int ROWS=4;
+		final int COLS=4;
+		
+		//Variables declaration
+		int cont=1;
+		
+		//Array 2D
+		int[][] matrix = new int[ROWS][COLS];
+		
+		for(int i=0; i<ROWS; i++)
+		{
+			for(int j=0; j<COLS; j++)
+			{
+				matrix[i][j]=cont;
+				cont++;
+			}
+		}
+		for(int i=0; i<ROWS; i++)
+		{
+			for(int j=0; j<COLS; j++)
+			{
+				System.out.print(matrix[i][j]+" ");
+			}
+			System.out.println(" ");
+		}
+	}
+
+}
